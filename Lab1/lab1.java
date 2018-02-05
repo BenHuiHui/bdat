0868c2f0e11d71d7e27da3e9b81bbbb91df2c29e
BenHuiHui <qinghe1992@gmail.com>
2018-02-05 22:35:00 +0800

    Initial commit

---
+++ Lab1/lab1.java
@@ -0,0 +1,20 @@
+import scala.Tuple2;
+
+import org.apache.spark.api.java.*;
+import org.apache.spark.api.java.function.*;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.SparkConf;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Collection;
+
+public class Lab1 {
+	public static void main(String[] args) {
+		private static String filePath = "AssignmentData/datafiles";
+		private static Char[] specialCharacters = [',', '.', '!', '[', ']'];
+
+		  
+	}
+}
