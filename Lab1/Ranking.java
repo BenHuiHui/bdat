43477d40d08f18a6b19c984bc33a3a9e8840cf7c
BenHuiHui <qinghe1992@gmail.com>
2018-02-06 21:07:09 +0800

    Rename

--- Lab1/Ranking.java
+++ Lab1/Ranking.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Collection;
 
-public final class Lab1 {
+public final class Ranking {
 	public static void main(String[] args) throws Exception {
 		private  String filePath = "AssignmentData/datafiles";
 		private  String stopWordFilePath = "AssignmentData/stopwords.txt";
